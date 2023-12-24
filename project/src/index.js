b3493a2b8846e2395bbf708579b0958f39c24992
SuperHeavyBallet <124462074+SuperHeavyBallet@users.noreply.github.com>
2023-12-24 04:03:55 +0100

    Initial commit

---
+++ project/src/index.js
@@ -0,0 +1,14 @@
+import assignEventListeners from "./assignEventListeners";
+import imageCarousel from "./imageCarousel";
+
+
+assignEventListeners(
+    ".menu-icon", 
+    ".menu-element",
+    ".dropdown-content", 
+    "dropdown-content-display", 
+    "dropdown-content-hidden" 
+    );
+
+    imageCarousel();
+
